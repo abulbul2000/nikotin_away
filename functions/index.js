5b47b0cfaf3401362df4be2abbf16b8cdfac46ab
abulbul2000 <3.04128626e+08+abulbul2000@users.noreply.github.com>
2026-08-16 16:34:26 +0000

    fix: remove duplicate function import

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,4 @@
 import { onCall, HttpsError } from "firebase-functions/v2/https";
-import { onCall, HttpsError } from "firebase-functions/v2/https";
 import { defineSecret } from "firebase-functions/params";
 import { initializeApp } from "firebase-admin/app";
 import { getFirestore, FieldValue } from "firebase-admin/firestore";
